9227c8774e610285b5d3546367c5480540624705
Shubham Sharma <81086110+Major0p@users.noreply.github.com>
2024-09-25 16:52:12 +0530

    my drive models

---
+++ FileStorageInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace core_Web_App.Models
+{
+    public class FileStorageInfo
+    {
+        [Key]
+        [Column("Id",TypeName ="varchar(16)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; }
+
+        [Column("Data",TypeName = "varbinary(max)")]
+        public byte[] Data { get; set; }    
+    }
+}
+
+
+
---
+++ User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace core_Web_App.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("UserId",TypeName = "nvarchar(20)")]
+        [StringLength(16,MinimumLength = 8,ErrorMessage = "UserId should be only 8 character")]
+        [Required]
+        [NotNull]
+        public string UserId { get; set; }
+
+        [Column("Password",TypeName = "nvarchar(20)")]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password should be only 8 character")]
+        [Required]
+        [NotNull]
+        public string Password { get; set; }
+
+        [Column("Name", TypeName = "nvarchar(max)")]
+        [Required]
+        [NotNull]
+        public string Name { get; set; }
+
+        [Column("Email", TypeName = "nvarchar(20)")]
+        public string Email { get; set; }
+
+        [Column("Profile",TypeName = "varbinary(max)")]
+        public byte[] Profile { get; set; }
+    }
+}
